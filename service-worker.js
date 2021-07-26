2149f763201ffff7bf64cdb1dbc7298974d2ee3e
Сакевич Анастасія <asakevych@ASMAC.local>
2021-07-26 18:27:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.35369277eabbfb9f41d8e1c2d600fc7c.js"
+  "./precache-manifest.62fcded7fb197bc6e65c706eb8d47756.js"
 );
 
 self.addEventListener('message', (event) => {
